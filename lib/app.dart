08b39f1da0f17b24bd54e4beedcd1298a1e7ecf4
rey0428 <hamada2002.0428@gmail.com>
2022-10-13 10:18:56 +0900

    一回目

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'view/auth/Top.dart';
 import 'view/auth/login.dart';
 import 'view/first.dart';
 
@@ -10,10 +11,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: First.id,
+      initialRoute: Top.id,
       routes: <String, WidgetBuilder>{
         First.id: (BuildContext context) => const First(),
         Login.id: (BuildContext context) => const Login(),
+        Top.id:(BuildContext context) => const Top(),
       },
     );
   }
--- lib/view/first.dart
+++ lib/view/first.dart
@@ -6,6 +6,7 @@ class First extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+    );
   }
 }
