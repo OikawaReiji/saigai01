db29dbc44af6493078192b6e23a2670769b7a016
OikawaReiji <r.oikawa.sys@morijyobi.ac.jp>
2022-10-20 10:32:05 +0900

    mmm

--- lib/view/auth/login.dart
+++ lib/view/auth/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:saigai01/compornet/auth/my_textfield.dart';
 
 import '../../provider/general_provider.dart';
 
@@ -9,121 +11,134 @@ class Login extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final size = MediaQuery.of(context).size;
     final authPageStateController =
         ref.watch(authSwitcherPriovider.notifier); //変更　関数の実行
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 63, 63, 63),
-      appBar: AppBar(
-        title: const Text(
-          "ログイン",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 50,
-          ),
-        ),
-        centerTitle: true,
-        leading: InkWell(
-          onTap: () {
-            authPageStateController.state = Pages.top;
-          },
-          child: const Icon(Icons.arrow_back_outlined),
-        ),
-        backgroundColor: const Color.fromARGB(255, 63, 63, 63),
-        elevation: 0.0,
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 100,
-              ),
-              Container(
-                width: double.infinity,
-                height: 70,
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                        color: const Color.fromARGB(255, 255, 255, 255)),
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    borderRadius: const BorderRadius.all(Radius.circular(100))),
-                child: Center(
-                  child: TextFormField(
-                    style: const TextStyle(fontSize: 25, color: Colors.white),
-                    decoration: const InputDecoration(
-                        icon: Icon(
-                          Icons.mail,
-                          size: 40,
-                          color: Color.fromARGB(255, 255, 255, 255),
+    final authController = ref.watch(authControllerProvider.notifier);
+    final maillContainer = useTextEditingController(text: "");
+    final passContainer = useTextEditingController(text: "");
+
+    final ValueNotifier<bool> loading = useState(false);
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: const Color.fromARGB(255, 63, 63, 63),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment(0, -0.5),
+                    child: SingleChildScrollView(
+                      child: SizedBox(
+                        height: 500,
+                        child: Column(
+                          children: [
+                            const SizedBox(
+                              height: 80,
+                            ),
+                            MyTextfield(
+                              title: 'email',
+                              icon: const Icon(
+                                Icons.mail,
+                                size: 40,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                              controller: maillContainer,
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            MyTextfield(
+                              title: 'password',
+                              icon: const Icon(
+                                Icons.lock,
+                                size: 40,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                              controller: passContainer,
+                            ),
+                            const Spacer(),
+                            InkWell(
+                              onTap: () async {
+                                FocusScope.of(context).unfocus();
+                                loading.value = true;
+                                try {
+                                  await authController.signin(
+                                      maillContainer.text,
+                                      passContainer.text,
+                                      loading);
+                                } catch (e) {
+                                  debugPrint(e.toString());
+                                }
+                              },
+                              child: Container(
+                                width: double.infinity,
+                                height: 65,
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: const Color.fromARGB(
+                                            255, 13, 245, 227),
+                                        width: 4),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(100))),
+                                child: const Center(
+                                  child: Text(
+                                    "ログイン",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 40,
+                                        color:
+                                            Color.fromARGB(255, 13, 245, 227)),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        hintText: "email",
-                        hintStyle: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255)),
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: double.infinity,
-                height: 70,
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    border: Border.all(
-                        color: const Color.fromARGB(255, 255, 255, 255)),
-                    borderRadius: const BorderRadius.all(Radius.circular(100))),
-                child: Center(
-                  child: TextFormField(
-                    style: const TextStyle(fontSize: 25, color: Colors.white),
-                    decoration: const InputDecoration(
-                        icon: Icon(
-                          Icons.lock,
-                          size: 40,
-                          color: Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        hintText: "password",
-                        hintStyle: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255)),
-                        border: InputBorder.none,
-                        focusedBorder: InputBorder.none),
-                  ),
-                ),
-              ),
-              const Spacer(),
-              InkWell(
-                onTap: () {
-                  debugPrint("破壊");
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 65,
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: const Color.fromARGB(255, 13, 245, 227),
-                          width: 4),
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(100))),
-                  child: const Center(
-                    child: Text(
-                      "ログイン",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 40,
-                          color: Color.fromARGB(255, 13, 245, 227)),
+                      ),
                     ),
                   ),
-                ),
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            authPageStateController.state = Pages.top;
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        const Text(
+                          "ログイン",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
